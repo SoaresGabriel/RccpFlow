7e91a528d25cf13b9af056dbf23a1faba77b9a97
Gabriel Soares <soaresggm@gmail.com>
2018-04-24 20:15:10 -0300

    Add MinCut algorithm

---
+++ Source/GraphUtils.h
@@ -0,0 +1,13 @@
+#ifndef GRAPHUTILS_H_
+#define GRAPHUTILS_H_
+
+#include <vector>
+#include <list>
+
+using namespace std;
+
+
+void getComponents(vector<vector<int> >& components, vector<list<int> >& adjList);
+void dfs(int v, vector<int> &component, vector<list<int>>& adjList, vector<int>& visited);
+
+#endif /* GRAPHUTILS_H_ */
---
+++ Source/MinCut.h
@@ -0,0 +1,8 @@
+#ifndef MINCUT_H_
+#define MINCUT_H_
+
+#include <vector>
+
+std::vector<std::vector<int> > minCut(std::vector<std::vector<double> > &graph, std::vector<int> &component);
+
+#endif /* MINCUT_H_ */
